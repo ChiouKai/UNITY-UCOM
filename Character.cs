53506e8ce20675454b038a9a69b02e9e50b5a867
ChiouKai <73415846+ChiouKai@users.noreply.github.com>
2020-11-20 12:33:24 +0800

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character :MonoBehaviour
+{
+    public int HP = 5;
+    public float Mobility= 8.0f;
+    public int Speed = 5;
+    public int AP = 0;
+    enum group
+    {
+        Human = 0,
+        Alien = 1
+    }
+    enum Type
+    {
+        Humanoid = 0,
+        Monster = 1,
+        Robet = 2
+    }
+
+}
---
+++ RoundSysytem.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundSysytem 
+{
+    private static RoundSysytem m_insatnce;
+    public static RoundSysytem GetInstance()
+    {
+        if (m_insatnce != null)
+        {
+            return m_insatnce;
+        }
+        else{
+            m_insatnce = new RoundSysytem();
+            return m_insatnce;
+        }
+
+    }
+
+    //GameObject[] Humans;
+    //GameObject[] Aliens;
+    public bool EndSignal = false;
+    List<(GameObject Cha, int Speed)> Sequence;
+    public void RoundPrepare(GameObject[] humans,GameObject[] aliens) 
+    {
+        
+        //Humans = humans;
+        //Aliens = aliens;
+        foreach (GameObject human in humans)
+        {
+            int Count = 0;
+            (GameObject Chr,int speed)obj = (human, human.GetComponent<Character>().Speed);
+            if (Sequence == null)
+            {
+                Sequence = new List<(GameObject, int)>();
+                Sequence.Add(obj);
+                continue;
+            }
+            foreach((_ ,int LSpeed) in Sequence)
+            {
+                if (obj.Item2 > LSpeed)
+                {
+                    Sequence.Insert(Count,obj);
+                }
+                ++Count;
+            }
+
+        }
+        foreach (GameObject alien in aliens)
+        {
+            int Count = 0;
+            (GameObject Chr, int speed) obj = (alien, alien.GetComponent<Character>().Speed);
+            if (Sequence == null)
+            {
+                Sequence = new List<(GameObject, int)>();
+                Sequence.Add(obj);
+                continue;
+            }
+            foreach ((_, int LSpeed) in Sequence)
+            {
+                if (obj.Item2 > LSpeed)
+                {
+                    Sequence.Insert(Count, obj);
+                }
+                ++Count;
+            }
+
+        }
+    }
+    public void RoundStart()
+    {
+        while (true)
+        {
+            GameObject[] ChaSeq= new GameObject[Sequence.Count];
+            //右邊順序動畫
+            for (int Count = 0; Count < Sequence.Count; ++Count)//載入角色順序
+            {
+                ChaSeq[Count] = Sequence[Count].Cha;
+            }
+            for(int Count =0; Count< ChaSeq.Length; ++Count)//開始行動
+            {
+                //ChaSeq[Count].searchenemy();
+                //ChaSeq[Count].CalMove();
+                ChaSeq[Count].GetComponent<Character>().AP = 2;
+                while (EndSignal != true)
+                {
+                    System.Threading.Thread.Sleep(1);
+                }
+                //while(動畫還沒結束)
+                //System.Threading.Thread.Sleep(1);
+            }
+            //事件?增援?newcome
+
+        }
+    }
+    private void NewCome()
+    {
+
+    }
+}
