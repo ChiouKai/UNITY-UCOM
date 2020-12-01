5e1ed0af6ed3c724ddd6a8c0844fa78cf8f9fd6d
ChiouKai <73415846+ChiouKai@users.noreply.github.com>
2020-12-01 23:39:04 +0800

    Update Character.cs

--- kai/Character.cs
+++ kai/Character.cs
@@ -1,19 +1,19 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character :MonoBehaviour
+public  class Character :MonoBehaviour
 {
     public int HP = 5;
-    public float Mobility= 8.0f;
+    public float Mobility= 8.0f;//移動距離
     public int Speed = 5;
-    public int AP = 0;
-    enum group
+
+    public enum group
     {
         Human = 0,
         Alien = 1
     }
-    enum Type
+    public enum Type
     {
         Humanoid = 0,
         Monster = 1,
