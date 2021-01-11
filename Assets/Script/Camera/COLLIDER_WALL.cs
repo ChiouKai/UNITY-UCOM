2cf8275f865e43610b17a420d06668c8a63c0261
JinYuKuo <hugo75342@gmail.com>
2021-01-11 17:15:04 +0800

    attack_camera

---
+++ Assets/Script/Camera/COLLIDER_WALL.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COLLIDER_WALL : MonoBehaviour
+{
+    public Material[] wall_render;
+    public Material[] wall_render_alpha;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hello : " +other.gameObject.name);
+        if (other.tag == "Wall")
+        {
+            other.GetComponent<Renderer>().materials = wall_render_alpha;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Wall")
+        {
+            other.GetComponent<Renderer>().materials = wall_render;
+        }
+    }
+}
